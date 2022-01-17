c87baa372e3971acae89cf247281e4bf8106d594
HJR <hjryu1816@gmail.com>
2022-01-17 15:29:09 +0900

    합병정렬 추가

--- Sorting.cpp
+++ Sorting.cpp
@@ -49,6 +49,42 @@ void qsort(vector<int> &v, int l, int r) {
         qsort(v,l+1,br);
 }
 
+// 차례대로 분할, 병합 함수
+int arr[1000001], arr2[1000001];
+void merge(int left, int right)
+{
+    int mid = (left + right) / 2;
+
+    int i = left;
+    int j = mid + 1;
+    int k = left;
+    while (i <= mid && j <= right)
+    {
+        if (arr[i] <= arr[j])
+            arr2[k++] = arr[i++];
+        else
+            arr2[k++] = arr[j++];
+    }
+
+    int tmp = i>mid ? j : i;
+
+    while(k<=right) arr2[k++] = arr[tmp++];
+
+    for (int i=left;i<=right;i++) arr[i] = arr2[i];
+}
+
+void partition(int left,int right)
+{
+    int mid;
+    if (left < right)
+    {
+        mid = (left + right) / 2;
+        partition(left, mid);
+        partition(mid + 1, right);
+        merge(left, right);
+    }
+}
+
 int main() {
     int n, i, a;
     vector<int> v;
@@ -57,10 +93,11 @@ int main() {
     for (i=0; i < n; i++) {
         cin >> a;
         v.push_back(a);
+        arr[i] = a;
     }
 
     int choice;
-    cout << "1. selectionSort\n" << "2. insertinSort\n" << "3. bubbleSort\n" << "4. quickSort\n";
+    cout << "1. selectionSort\n" << "2. insertinSort\n" << "3. bubbleSort\n" << "4. quickSort\n" << "5. mergeSort\n";
     cout << "정렬 알고리즘 선택 >>> ";
     cin >> choice;
 
@@ -78,15 +115,23 @@ int main() {
             cout << "quickSort\n";
             qsort(v, 0, v.size()-1);
             break;
+        case 5:
+            cout << "mergeSort\n";
+            partition(0, n-1);
+            break;
         default:
             cout << "정렬 취소" << endl;
             break;
     }
-
-    for (int i=0; i < v.size(); i++)
-        cout << v[i] << " ";
-    cout << endl;
-
+    if (choice < 5) {
+        for (int i=0; i < n; i++)
+            cout << v[i] << " ";
+        cout << endl;
+    } else {
+        for (int i=0; i < n; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
     return 0;
 }
 
